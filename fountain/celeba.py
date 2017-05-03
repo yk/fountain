ddd0fc9b0a37dd4440d7c1e0d9d9a4df7311206e
Yannic Kilcher <yannic.kilcher@gmail.com>
2017-05-03 13:27:04 +0200

    resize & crop

--- fountain/celeba.py
+++ fountain/celeba.py
@@ -98,3 +98,5 @@ if __name__ == '__main__':
         print(CelebA(200, resize=[64, 64], crop=[108, 108]).create_queue())
         print(CelebA(200, resize=[32, 32], crop=[138, 138]).create_queue())
         print(CelebA(200, resize=[64, 64], crop=[138, 138]).create_queue())
+        print(CelebA(200, resize=[32, 32], crop=[118, 118]).create_queue())
+        print(CelebA(200, resize=[64, 64], crop=[118, 118]).create_queue())
