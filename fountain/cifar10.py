452d5951b126e584c670c946d0bddcc786a7ae76
Yannic Kilcher <yannic.kilcher@gmail.com>
2017-05-10 19:46:56 +0200

    cifar100

--- fountain/cifar10.py
+++ fountain/cifar10.py
@@ -8,24 +8,32 @@ import _pickle as cPickle
 IMG_SHAPE = [32, 32, 3]
 
 class CIFAR10(LabeledImageMixin, Dataset):
-    def __init__(self, tfrecord=True, mode='train'):
+    def __init__(self, tfrecord=True, mode='train', num_classes=10):
         super().__init__(mode=mode)
         self.isTf = tfrecord
+        assert num_classes == 10 or num_classes == 100
+        self.num_classes = num_classes
+
+    def name(self):
+        return 'cifar{}'.format(self.num_classes)
 
     def get_size(self):
         return 50000 if self.mode == 'train' else 10000
 
     def files(self):
-        with sub_path('cifar10'):
-            tar = GzippedFile('cifar10.tar', OnlineFile('cifar10.tar.gz', 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'))
-            batches = [TaredFile('cifar10_b{}.pkl'.format(i), 'cifar-10-batches-py/data_batch_{}'.format(i), tar) for i in range(1, 6)] + [TaredFile('cifar10_test', 'cifar-10-batches-py/test_batch', tar)]
+        with sub_path(self.get_sub_path()):
+            tar = GzippedFile('cifar{}.tar'.format(self.num_classes), OnlineFile('cifar{}.tar.gz'.format(self.num_classes), 'https://www.cs.toronto.edu/~kriz/cifar-{}-python.tar.gz'.format(self.num_classes)))
+            if self.num_classes == 10:
+                batches = [TaredFile('cifar{}_b{}.pkl'.format(self.num_classes, i), 'cifar-{}-batches-py/data_batch_{}'.format(self.num_classes, i), tar) for i in range(1, 6)] + [TaredFile('cifar{}_test'.format(self.num_classes), 'cifar-{}-batches-py/test_batch'.format(self.num_classes), tar)]
+            else:
+                batches = [TaredFile('cifar100_train.pkl', 'cifar-100-python/train', tar), TaredFile('cifar100_test.pkl', 'cifar-100-python/test', tar)]
             if not self.isTf:
-                files = [self.CIFAR10DataFile('cifar10_images.npy', False, batches), self.CIFAR10DataFile('cifar10_labels.npy', True, batches)]
+                files = [self.CIFAR10DataFile('cifar{}_images.npy'.format(self.num_classes), self.num_classes, False, batches), self.CIFAR10DataFile('cifar{}_labels.npy'.format(self.num_classes), self.num_classes, True, batches)]
             else:
                 if self.mode == 'train':
-                    files = [self.CIFAR10DataFile('cifar10.train.tfrecords', None, batches[:-1])]
+                    files = [self.CIFAR10DataFile('cifar{}.train.tfrecords'.format(self.num_classes), self.num_classes, None, batches[:-1])]
                 else:
-                    files = [self.CIFAR10DataFile('cifar10.test.tfrecords', None, batches[-1:])]
+                    files = [self.CIFAR10DataFile('cifar{}.test.tfrecords'.format(self.num_classes), self.num_classes, None, batches[-1:])]
             return files
 
     def get_data_raw(self):
@@ -49,19 +57,21 @@ class CIFAR10(LabeledImageMixin, Dataset):
         return image, label
 
     class CIFAR10DataFile(File):
-        def __init__(self, name, isLabels, batches):
+        def __init__(self, name, num_classes, isLabels, batches):
             super().__init__(name, batches)
             self.isLabels = isLabels
             self.isTf = name.endswith('tfrecords')
+            self.num_classes = num_classes
 
         def update(self):
+            labels_name = 'labels' if self.num_classes == 10 else 'fine_labels'
             if not self.isTf:
                 data = []
                 for b in self.dependencies:
                     with open(b.path, 'rb') as f:
                         d = cPickle.load(f, encoding='latin1')
                     if self.isLabels:
-                        data.extend(d['labels'])
+                        data.extend(d[labels_name])
                     else:
                         data.extend(d['data'])
                 if self.isLabels:
@@ -75,7 +85,7 @@ class CIFAR10(LabeledImageMixin, Dataset):
                     for b in self.dependencies:
                         with open(b.path, 'rb') as f:
                             ex = cPickle.load(f, encoding='latin1')
-                        ds, ls = np.array(ex['data'], np.uint8), np.array(ex['labels'], np.int64)
+                        ds, ls = np.array(ex['data'], np.uint8), np.array(ex[labels_name], np.int64)
                         ds = ds.reshape((-1, 3, 32, 32)).transpose((0, 2, 3, 1)).reshape((-1, 32 * 32 * 3))
                         for d, l in zip(ds, ls):
                             image_raw = d.tostring()
@@ -89,3 +99,4 @@ class CIFAR10(LabeledImageMixin, Dataset):
 if __name__ == '__main__':
     print(CIFAR10(tfrecord=False).get_data()[0].shape)
     print(CIFAR10().create_queue())
+    print(CIFAR10(num_classes=100).create_queue())
