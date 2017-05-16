e75fd7132633f57e6cc4484a772c6f1e51b69309
Yannic Kilcher <yannic.kilcher@gmail.com>
2017-05-16 11:18:57 +0200

    weights

--- fountain/mnist1k.py
+++ fountain/mnist1k.py
@@ -104,5 +104,5 @@ class MNIST1K(LabeledImageMixin, Dataset):
                     writer.write(example.SerializeToString())
 
 if __name__ == '__main__':
-    print(MNIST1K(num_digits=2, num_blocks=400).create_queue())
-    print(MNIST1K(num_digits=3, num_blocks=400).create_queue())
+    # print(MNIST1K(num_digits=2, num_blocks=400).create_queue())
+    print(MNIST1K(num_digits=3, num_blocks=100).create_queue())
